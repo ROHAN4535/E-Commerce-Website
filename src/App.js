d0b36636068ec4e197e47c329df8125c3b0e9253
ROHAN4535 <grohan468@gmail.com>
2023-11-07 15:56:26 +0530

    Making Add to Cart Button Work

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import Navbar from "./Components/Navbar/Navbar";
 import Header from "./Components/Header/Header";
 import Store from "./Components/Store/Store";
 import Footer from "./Components/Footer/Footer";
+import CartProvider from "./Components/Context/CartProvider";
 
 function App() {
   return (
-    <div>
+    <CartProvider>
       <Navbar />
       <Header />
-      <Store/>
-      <Footer/>
-    </div>
+      <main>
+        <Store />
+      </main>
+      <Footer />
+    </CartProvider>
   );
 }
 export default App;
--- src/Components/Store/Store.js
+++ src/Components/Store/Store.js
@@ -1,4 +1,4 @@
-import React from "react";
+import {useContext} from "react";
 import { Card, Button, Container, Row, Col } from "react-bootstrap";
 import Album1 from "../../Assets/Music/Album 1.png";
 import Album2 from "../../Assets/Music/Album 2.png";
@@ -6,24 +6,29 @@ import Album3 from "../../Assets/Music/Album 3.png";
 import Album4 from "../../Assets/Music/Album 4.png";
 import Tshirt from "../../Assets/Merch/Shirt.png";
 import Cofee from "../../Assets/Merch/Cofee.png";
+import CartContext from "../Context/cart-context";
 
 const albums = [
   {
+    id: "m1",
     name: "Album 1",
     image: Album1,
     price: 12.99,
   },
   {
+    id: "m2",
     name: "Album 2",
     image: Album2,
     price: 14.99,
   },
   {
+    id: "m3",
     name: "Album 3",
     image: Album3,
     price: 9.99,
   },
   {
+    id: "m4",
     name: "Album 4",
     image: Album4,
     price: 19.99,
@@ -32,11 +37,13 @@ const albums = [
 
 const merchs = [
   {
+    id: "m5",
     name: "T-Shirt",
     image: Tshirt,
     price: 19.99,
   },
   {
+    id: "m6",
     name: "Cofee Cup",
     image: Cofee,
     price:6.99,
@@ -44,6 +51,10 @@ const merchs = [
 ];
 
 const Store = () => {
+  const cartCntx = useContext(CartContext);
+  const addToCartHandler = (item) => {
+    cartCntx.addItem({...item,quantity:1});
+  }
   return (
     <div>
       <Container>
@@ -59,7 +70,7 @@ const Store = () => {
                 <Card.Footer>
                 <div className="d-flex justify-content-between align-items-center">
                   <span>${album.price.toFixed(2)}</span>
-                  <Button variant="primary">Add To Cart</Button>
+                  <Button variant="primary" onClick={() => addToCartHandler(album)}>Add To Cart</Button>
                   </div>
                 </Card.Footer>
               </Card>
@@ -81,7 +92,7 @@ const Store = () => {
                 <Card.Footer>
                 <div className="d-flex justify-content-between align-items-center">
                   <span>${merch.price.toFixed(2)}</span>
-                  <Button variant="primary">Add To Cart</Button>
+                  <Button variant="primary" onClick={() => addToCartHandler(merch)}>Add To Cart</Button>
                   </div>
                 </Card.Footer>
               </Card>
