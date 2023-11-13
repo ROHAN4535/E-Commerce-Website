551771c1f0d1220ce5edc7b189f43e42d1d43b89
ROHAN4535 <grohan468@gmail.com>
2023-11-13 20:19:01 +0530

    Implement the Products Page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,86 @@
 import Store from "./Components/Store/Store";
-import About from "./Components/About/About";
-import CartProvider from "./Components/Context/CartProvider";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import RootLayout from "./Components/Layout/Root";
 import Home from "./Components/Home/Home";
+import About from "./Components/About/About";
 import ContactUs from "./Components/Contact/ContactUs";
+import CartProvider from "./Components/Context/CartProvider";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import RootLayout from "./Components/Layout/Root";
+import Product from "./Components/Store/Product";
 
-const router = createBrowserRouter([
+const productsArr = [
   {
-    path: "/",
-    element: <RootLayout />,
-    children: [
-      { path: "/", element: <Home/> },
-      { path: "/store", element: <Store /> },
-      { path: "/about", element: <About /> },
-      { path: "/contact",element:<ContactUs/>}
+    title: "Album 1",
+    price: 100,
+    imageUrl: [
+      "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+      "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
     ],
+
+    des: "Lorem ipsum carrots enhanced rebates. Excellent sayings of a man of sorrows",
   },
-]);
+
+  {
+    title: "Album 2",
+    price: 50,
+    imageUrl: [
+      "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+    ],
+    des: "Lorem ipsum carrots enhanced rebates. Excellent sayings of a man of sorrows",
+  },
+
+  {
+    title: "Album 3",
+    price: 70,
+    imageUrl: [
+      "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+    ],
+    des: "Lorem ipsum carrots enhanced rebates. Excellent sayings of a man of sorrows",
+  },
+
+  {
+    title: "Album 4",
+    price: 100,
+    imageUrl: [
+      "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
+    ],
+    des: "Lorem ipsum carrots enhanced rebates. Excellent sayings of a man of sorrows",
+  },
+
+];
+
 
 function App() {
-  return <CartProvider>
-    <RouterProvider router={router}/>
-  </CartProvider>;
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <RootLayout />,
+  //     children: [
+  //       { path: "/", element: <Home /> },
+  //       { path: "/store", element: <Store /> },
+  //       { path: "/about", element: <About /> },
+  //       {path: "/contact",element:<ContactUS/>}
+  //     ],
+  //   },
+  // ]);
+
+  return (
+    <CartProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<RootLayout />}>
+            <Route index element={<Home />} />
+            <Route path="store" element={<Store productsArr={productsArr}/>} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<ContactUs />} />
+            <Route
+              path="store/:productId"
+              element={<Product productsArr={productsArr} />}
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </CartProvider>
+  );
 }
 
 export default App;
--- src/Components/Store/Store.js
+++ src/Components/Store/Store.js
@@ -1,111 +1,45 @@
-import {useContext} from "react";
+import { useContext } from "react";
 import { Card, Button, Container, Row, Col } from "react-bootstrap";
-import Album1 from "../../Assets/Music/Album 1.png";
-import Album2 from "../../Assets/Music/Album 2.png";
-import Album3 from "../../Assets/Music/Album 3.png";
-import Album4 from "../../Assets/Music/Album 4.png";
-import Tshirt from "../../Assets/Merch/Shirt.png";
-import Cofee from "../../Assets/Merch/Cofee.png";
+import { Link } from "react-router-dom";
 import CartContext from "../Context/cart-context";
-import classes from './Store.module.css';
+import classes from "./Store.module.css";
 
-const albums = [
-  {
-    id: "m1",
-    name: "Album 1",
-    image: Album1,
-    price: 12.99,
-  },
-  {
-    id: "m2",
-    name: "Album 2",
-    image: Album2,
-    price: 14.99,
-  },
-  {
-    id: "m3",
-    name: "Album 3",
-    image: Album3,
-    price: 9.99,
-  },
-  {
-    id: "m4",
-    name: "Album 4",
-    image: Album4,
-    price: 19.99,
-  },
-];
-
-const merchs = [
-  {
-    id: "m5",
-    name: "T-Shirt",
-    image: Tshirt,
-    price: 19.99,
-  },
-  {
-    id: "m6",
-    name: "Cofee Cup",
-    image: Cofee,
-    price:6.99,
-  }
-];
-
-const Store = () => {
+const Store = (props) => {
   const cartCntx = useContext(CartContext);
   const addToCartHandler = (item) => {
-    cartCntx.addItem({...item,quantity:1});
-  }
+    cartCntx.addItem({ ...item, quantity: 1 });
+  };
+
   return (
     <div>
       <Container className={classes.container}>
         <h3>Music</h3>
         <Row xs={1} md={2} className="g-8">
-          {albums.map((album, index) => (
+          {props.productsArr.map((item, index) => (
             <Col key={index}>
               <Card className={classes.card}>
-                <Card.Header className={classes.name}>{album.name}</Card.Header>
-                <Card.Body className={classes.cardbody}>
-                  <Card.Img src={album.image} className={classes.img} />
+                <Link to={`/store/${item.title}`}>
+                  <Card.Title className={classes.title}>{item.title}</Card.Title>
+                  <Card.Img src={item.imageUrl[0]} className={classes.img} />
+                </Link>
+                <Card.Body className={classes.footer}>
+                  <span>${item.price.toFixed(2)}</span>
+                  <Button
+                    variant="primary"
+                    onClick={() => addToCartHandler(item)}
+                  >
+                    Add To Cart
+                  </Button>
                 </Card.Body>
-                <Card.Footer className={classes.footer}>
-                <div className="d-flex justify-content-between align-items-center">
-                  <span>${album.price.toFixed(2)}</span>
-                  <Button variant="primary" onClick={() => addToCartHandler(album)}>Add To Cart</Button>
-                  </div>
-                </Card.Footer>
               </Card>
             </Col>
           ))}
         </Row>
       </Container>
-
-      <Container className={classes.container}>
-        <h3>Merch</h3>
-        <Row xs={1} md={2} className="g-8">
-          {merchs.map((merch, index) => (
-            <Col key={index}>
-              <Card className={classes.card}>
-              <Card.Header className={classes.name}>{merch.name}</Card.Header>
-                <Card.Body>
-                  <Card.Img src={merch.image} />
-                </Card.Body>
-                <Card.Footer className={classes.img}>
-                <div className="d-flex justify-content-between align-items-center">
-                  <span>${merch.price.toFixed(2)}</span>
-                  <Button variant="primary" onClick={() => addToCartHandler(merch)}>Add To Cart</Button>
-                  </div>
-                </Card.Footer>
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      </Container>
-      <div className="text-center">
-      <Button variant="secondary" className={classes.seeCartBtn}>See the cart</Button>
-      </div>
+      <Button variant="secondary" className={classes.seeCartBtn}>
+        See the cart
+      </Button>
     </div>
   );
 };
-
 export default Store;
